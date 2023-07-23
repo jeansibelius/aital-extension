80972ce103d23976da91ffad82ef8eb2a63f9cb9
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2023-07-23 20:24:33 +0900

    Add receiving updated position & refactors

--- src/script.js
+++ src/script.js
@@ -1,195 +1,229 @@
 //@ts-check
-
 import "./styles.scss";
 
-// Create UI
-const details = document.createElement("details");
-details.open = true;
-details.dataset.id = "aital";
-const summary = document.createElement("summary");
-summary.innerHTML = `
-    <span>Am I Talking A Lot?</span>
-    <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="208 96 128 176 48 96" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg>
-    <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="48 160 128 80 208 160" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg>
-  `;
-details.appendChild(summary);
-const content = document.createElement("div");
-details.appendChild(content);
-document.body.appendChild(details);
-
-window.__aitalDebugMode = false;
-
-function onGot(item) {
+/**
+ * @param {{ aitalPosition: { x: number; y: number; }; }} item
+ */
+const updatePosition = (item) => {
   let [x, y] = [50, 10];
   if (item.aitalPosition) {
     x = item.aitalPosition.x;
     y = item.aitalPosition.y;
   }
-  details.style.left = `${x}%`;
-  details.style.top = `${y}%`;
-}
-
-const getting = browser.storage.sync.get("aitalPosition");
-getting.then(onGot, (e) => console.error("AITAL error: could not get position from storage"));
-
-const tally = {
-  __seconds: 0,
+  const details = document.getElementById("aital");
+  if (details) {
+    details.style.left = `${x}%`;
+    details.style.top = `${y}%`;
+  }
 };
-const elementsByName = {};
-// Declared here so that we can reference the values when debugging
-let indicators;
-let participants;
 
-function debug() {
-  if (window.__aitalDebugMode) {
-    console.log("🪲 AITAL", ...arguments, {
-      data: tally,
-      elements: elementsByName,
-      participants,
-      indicators,
-    });
-  }
-}
-
-function truncate(name) {
-  const parts = name.split(" ");
-  if (parts.length === 1) return name;
-
-  return parts
-    .map((part, i) => {
-      if (i === 0) return part;
-      return part[0] + ".";
-    })
-    .join(" ");
-}
-
-function valueToLevel(value) {
-  if (value >= 90) {
-    return 3;
-  } else if (value >= 75) {
-    return 2;
-  } else if (value >= 50) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-function tick() {
-  participants = document.querySelectorAll("[data-self-name]");
-
-  if (participants.length <= 1) {
+const runScript = () => {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
     return;
   }
+  window.hasRun = true;
 
-  tally.__seconds++;
+  // Create UI
+  const details = document.createElement("details");
+  details.open = true;
+  details.id = "aital";
+  details.dataset.id = "aital";
+  const summary = document.createElement("summary");
+  summary.innerHTML = `
+    <span>Am I Talking A Lot?</span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="208 96 128 176 48 96" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="48 160 128 80 208 160" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg>
+  `;
+  details.appendChild(summary);
+  const content = document.createElement("div");
+  details.appendChild(content);
 
-  indicators = [];
+  browser.storage.sync
+    .get("aitalPosition")
+    .then(updatePosition)
+    .catch((e) => console.error("AITAL error: could not get position from storage", e));
 
-  document.querySelectorAll("i.google-material-icons").forEach((node) => {
-    if (node.textContent === "devices") {
-      const previous = node.previousSibling;
-
-      if (!previous) {
-        // There should always be a previous sibling, but better safe than
-        // sorry.
-        debug("No previous sibling for node", node);
-        return;
-      }
-
-      if (previous.dataset.useTooltip) {
-        // There is an extra element wrapper for your video, which we can
-        // identify with data-use-tooltip.
-        indicators.push(previous.lastChild);
-      } else {
-        indicators.push(previous);
-      }
+  /**
+   * Listen for messages from the background script.
+   * Call "updatePosition()".
+   */
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "aitalUpdatePosition") {
+      updatePosition(message);
+      return Promise.resolve();
     }
   });
 
-  indicators.forEach((node, i) => {
-    const participant = participants[i];
+  document.body.appendChild(details);
 
-    if (!participant) {
-      debug("No matching participant for indicator", {
-        index: i,
-        indicators,
-        node,
+  window.__aitalDebugMode = false;
+
+  const tally = {
+    __seconds: 0,
+  };
+  const elementsByName = {};
+  // Declared here so that we can reference the values when debugging
+  let indicators;
+  let participants;
+
+  function debug() {
+    if (window.__aitalDebugMode) {
+      console.log("🪲 AITAL", ...arguments, {
+        data: tally,
+        elements: elementsByName,
         participants,
+        indicators,
       });
+    }
+  }
+
+  function truncate(name) {
+    const parts = name.split(" ");
+    if (parts.length === 1) return name;
+
+    return parts
+      .map((part, i) => {
+        if (i === 0) return part;
+        return part[0] + ".";
+      })
+      .join(" ");
+  }
+
+  function valueToLevel(value) {
+    if (value >= 90) {
+      return 3;
+    } else if (value >= 75) {
+      return 2;
+    } else if (value >= 50) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  function tick() {
+    participants = document.querySelectorAll("[data-self-name]");
+
+    if (participants.length <= 1) {
       return;
     }
 
-    const name = participant.textContent;
+    tally.__seconds++;
 
-    if (name.includes("(Present")) {
-      debug("Unexpected participant: " + name, {
-        indicators,
-        participants,
-      });
-    }
+    indicators = [];
 
-    if (!tally.hasOwnProperty(name)) {
-      debug(`Logging new participant: ${name}`);
-      tally[name] = 0;
-    }
+    document.querySelectorAll("i.google-material-icons").forEach((node) => {
+      if (node.textContent === "devices") {
+        const previous = node.previousSibling;
 
-    // If the second bar in the indicator is taller than 4px, then we know
-    // that the participant is talking.
-    // If they are muted, the value will be 0.
-    // If they are unmuted and not talking, the value will be 4.
-    if (node.children[1].getBoundingClientRect().height > 4) {
-      debug(`Talking: ${name}`);
-      tally[name]++;
-    }
-  });
+        if (!previous) {
+          // There should always be a previous sibling, but better safe than
+          // sorry.
+          debug("No previous sibling for node", node);
+          return;
+        }
 
-  const { __seconds, ...record } = tally;
+        if (previous.dataset.useTooltip) {
+          // There is an extra element wrapper for your video, which we can
+          // identify with data-use-tooltip.
+          indicators.push(previous.lastChild);
+        } else {
+          indicators.push(previous);
+        }
+      }
+    });
 
-  Object.entries(record).forEach(([name, count]) => {
-    const value = (count * 100) / __seconds;
-    const readableValue = value.toFixed(2);
+    indicators.forEach((node, i) => {
+      const participant = participants[i];
 
-    if (elementsByName[name]) {
-      // We have DOM references for this participant, so just update values
-      debug(`Update UI: ${name}`);
-      const { progress, percent } = elementsByName[name];
-      progress.value = value;
-      progress.dataset.level = valueToLevel(value);
-      percent.textContent = readableValue;
-    } else {
-      debug(`Create UI: ${name}`);
-      // No DOM references, create UI elements and store them
-      const label = document.createElement("label");
-      label.textContent = truncate(name);
-      label.title = name;
+      if (!participant) {
+        debug("No matching participant for indicator", {
+          index: i,
+          indicators,
+          node,
+          participants,
+        });
+        return;
+      }
 
-      const progress = document.createElement("progress");
-      progress.max = 100;
-      progress.value = value;
-      progress.dataset.level = valueToLevel(value).toString();
+      const name = participant.textContent;
 
-      const percent = document.createElement("span");
-      percent.textContent = readableValue;
+      if (name.includes("(Present")) {
+        debug("Unexpected participant: " + name, {
+          indicators,
+          participants,
+        });
+      }
 
-      content.appendChild(label);
-      content.appendChild(progress);
-      content.appendChild(percent);
+      if (!tally.hasOwnProperty(name)) {
+        debug(`Logging new participant: ${name}`);
+        tally[name] = 0;
+      }
 
-      elementsByName[name] = {
-        label,
-        percent,
-        progress,
-      };
-    }
-  });
-}
+      // If the second bar in the indicator is taller than 4px, then we know
+      // that the participant is talking.
+      // If they are muted, the value will be 0.
+      // If they are unmuted and not talking, the value will be 4.
+      if (node.children[1].getBoundingClientRect().height > 4) {
+        debug(`Talking: ${name}`);
+        tally[name]++;
+      }
+    });
 
-// Add a click handler for debugging purposes
-content.addEventListener("click", (e) => {
-  if (e.shiftKey) {
-    window.__aitalDebugMode = !window.__aitalDebugMode;
+    const { __seconds, ...record } = tally;
+
+    Object.entries(record).forEach(([name, count]) => {
+      const value = (count * 100) / __seconds;
+      const readableValue = value.toFixed(2);
+
+      if (elementsByName[name]) {
+        // We have DOM references for this participant, so just update values
+        debug(`Update UI: ${name}`);
+        const { progress, percent } = elementsByName[name];
+        progress.value = value;
+        progress.dataset.level = valueToLevel(value);
+        percent.textContent = readableValue;
+      } else {
+        debug(`Create UI: ${name}`);
+        // No DOM references, create UI elements and store them
+        const label = document.createElement("label");
+        label.textContent = truncate(name);
+        label.title = name;
+
+        const progress = document.createElement("progress");
+        progress.max = 100;
+        progress.value = value;
+        progress.dataset.level = valueToLevel(value).toString();
+
+        const percent = document.createElement("span");
+        percent.textContent = readableValue;
+
+        content.appendChild(label);
+        content.appendChild(progress);
+        content.appendChild(percent);
+
+        elementsByName[name] = {
+          label,
+          percent,
+          progress,
+        };
+      }
+    });
   }
-});
 
-setInterval(tick, 1000);
+  // Add a click handler for debugging purposes
+  content.addEventListener("click", (e) => {
+    if (e.shiftKey) {
+      window.__aitalDebugMode = !window.__aitalDebugMode;
+    }
+  });
+
+  setInterval(tick, 1000);
+};
+
+runScript();
